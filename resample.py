1e148a42383b546df538ba53ca3fc69fee21d625
y-ren16 <2367050142@qq.com>
2024-02-23 22:05:23 +0800

    1130-1300 stop

--- resample.py
+++ resample.py
@@ -32,10 +32,14 @@ def generate_sample_times_optimized(start_time, end_time, interval_in_seconds):
 def vectorized_resample(df, sampling_interval, on='eventTime'):
     # 创建采样时间点
     start_time = 93000000.0
-    end_time = df[on].iloc[-1]
-    # pdb.set_trace()
-    # sample_times = np.arange(start_time, end_time, sampling_interval)
-    sample_times = generate_sample_times_optimized(start_time, end_time, sampling_interval)
+    stop_time = 113000000.0
+    begain_time = 130000000.0
+    end_time = 145900000.0
+    
+    
+    sample_times1 = generate_sample_times_optimized(start_time, stop_time, sampling_interval)
+    sample_times2 = generate_sample_times_optimized(begain_time, end_time, sampling_interval)
+    sample_times = sample_times1 + sample_times2
     sample_times = np.array(sample_times)
 
     # 找到每个采样时间点最接近的索引
@@ -60,64 +64,21 @@ def vectorized_resample(df, sampling_interval, on='eventTime'):
     return resampled_df
 
 
-def df_resample(df, sampling_interval, on='eventTime'):
-    # 生成采样点时间戳
-    start_time = 93000000.0
-    end_time = df[on].iloc[-1]
-    sample_times = np.arange(start_time, end_time, sampling_interval)
-    # print(len(sample_times))
-
-    # 准备采样结果容器
-    sampled_indices = []
-    current_sample_index = 0
-    cache = None
-    cache_index = 0
-
-    # 对于DataFrame中的每一行
-    for index, row in df.iterrows():
-        if current_sample_index >= len(sample_times):
-            break
-        # 当前行的时间戳
-        current_time = row[on]
-        # 缓存上一行内容
-        if cache is None:
-            cache = row
-            cache_index = index
-        if current_time >= sample_times[current_sample_index]:
-            diff_pre = sample_times[current_sample_index] - cache[on]
-            diff_next = current_time - sample_times[current_sample_index]
-            if diff_next < diff_pre:
-                sampled_indices.append(index)
-            else:
-                sampled_indices.append(cache_index)
-            current_sample_index += 1
-        cache = row
-        cache_index = index
-
-    # 使用找到的索引从原始DataFrame中选择行
-    resampled_df = df.loc[sampled_indices].copy()
-
-    # 重置索引
-    resampled_df.reset_index(drop=True, inplace=True)
-
-    return resampled_df
-
-
 if __name__ == "__main__":
     stock_path = "./env/stock_raw"
-    signal_file_original_rootpath = os.path.join(stock_path, 'data1')
+    signal_file_original_rootpath = os.path.join(stock_path, 'data.bkp')
     dateList = [name for name in os.listdir(signal_file_original_rootpath) if
                 os.path.isdir(os.path.join(signal_file_original_rootpath, name))]
     print(dateList)
     os.makedirs(os.path.join(stock_path, "./data_resampled"), exist_ok=True)
     for date in tqdm(dateList[:]):
-    # for date in ['20200102']:
         print(date)
-        if date == '20200225' or date == '20200224' or date == '20200221' or date == '20200220':
+        if date in ['20200225', '20200224', '20200221', '20200220']:
+            print(f'skip {date}')
             continue
         file = ParquetFile()
         os.makedirs(os.path.join(stock_path, "./data_resampled/" + date), exist_ok=True)
-        file.filename = os.path.join(stock_path, "./data1/" + date + '/train_data.parquet')
+        file.filename = os.path.join(stock_path, "./data.bkp/" + date + '/train_data.parquet')
         file.load()
         df_ori = file.data
         code_list = df_ori['code'].unique()
@@ -125,29 +86,23 @@ if __name__ == "__main__":
         print(len(code_list))
         # assert len(code_list) == 496
         # assert len(code_list) == 100
-        # code_nums_each = []
-        # for code in tqdm(code_list):
-        #     df = df_ori[df_ori['code'] == code]
-        #     code_nums_each.append(len(df))
-        #     # start_time = 93000000.0
-        #     # end_time = 145900000.0
-        #     try:
-        #         df.iloc[0]["serverTime"] > 93000000.0
-        #     except:
-        #         print(df.iloc[0]["serverTime"])
-        #         print(code)
-        #
-        #     # assert df.iloc[-1]["serverTime"] < 145900000.0
-        #
-        #     # resampled df ever 5 seconds
-        #     # df_resampled = df_resample(df, sampling_interval=5000.0, on='serverTime')
-        #     df_resampled = vectorized_resample(df, sampling_interval=5, on='eventTime')
-        #
-        #     new_file = ParquetFile()
-        #     new_file.filename = os.path.join(stock_path,
-        #                                      "./data_resampled/" + date + '/train_data_' + str(int(code)) + '.parquet')
-        #     new_file.data = df_resampled
-        #     new_file.dump()
-        #     continue
-        # assert sum(code_nums_each) == len(df_ori)
+        code_nums_each = []
+        for code in tqdm(code_list):
+            df = df_ori[df_ori['code'] == code]
+            code_nums_each.append(len(df))
+            # try:
+            #     df.iloc[0]["serverTime"] > 93000000.0
+            # except:
+            #     print(df.iloc[0]["serverTime"])
+            #     print(code)
+        
+            df_resampled = vectorized_resample(df, sampling_interval=5, on='eventTime')
+        
+            new_file = ParquetFile()
+            new_file.filename = os.path.join(stock_path,
+                                             "./data_resampled/" + date + '/train_data_' + str(int(code)) + '.parquet')
+            new_file.data = df_resampled
+            new_file.dump()
+            continue
+        assert sum(code_nums_each) == len(df_ori)
         continue
